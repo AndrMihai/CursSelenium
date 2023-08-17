c144e106cea1884f1042a39cf74820d309c44e62
Xellos W <andrei.mihai3110@gmail.com>
2023-08-17 09:10:59 +0300

    tema18

--- src/main/java/Homework/Tema18.java
+++ src/main/java/Homework/Tema18.java
@@ -1,6 +1,8 @@
 package Homework;
 
 import utils.BaseTest;
+
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.By;
@@ -11,16 +13,18 @@ public class Tema18 extends BaseTest {
 	
 	
 	@Test
-	public void unoMetodo() {
+	public void unoMetodo() throws InterruptedException {
 		
 		WebElement login = driver.findElement(By.className("menu_user_login"));
 		assertTrue(login.isDisplayed());
-		driver.findElement(By.className("menu_user_login")).click();
 		WebElement loginField = driver.findElement(By.id("log"));
-		assertTrue(loginField.isDisplayed());
 		WebElement passField = driver.findElement(By.id("password"));
+		assertFalse(loginField.isDisplayed());
+		assertFalse(passField.isDisplayed());
+		driver.findElement(By.className("menu_user_login")).click();
+		Thread.sleep(3000);
+		assertTrue(loginField.isDisplayed());
 		assertTrue(passField.isDisplayed());
-		
 	}
 
 }
